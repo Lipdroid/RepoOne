cf9ad6422a045c76edb268a33129bc80b581a825
Md Munir Hossain <lipuhossain67@gmail.com>
2018-05-11 16:27:56 +0600

    update to test upstream

--- app/src/main/java/com/lipdroid/repoone/MainActivity.java
+++ app/src/main/java/com/lipdroid/repoone/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String testName = "Test Git Upstrem";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
